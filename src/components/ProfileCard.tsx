80b1aa81235e1ea11a7c08350dba6b4c0346b411
Brice Duke <bricewduke@gmail.com>
2023-05-25 10:15:22 -0500

    refactor(global): move profile card out

---
+++ src/components/ProfileCard.tsx
@@ -0,0 +1,88 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import { AppRouter } from '~/server/api/root';
+
+import { Hoodie, Pants, Person, Share, Sneaker, TShirt, Watch } from '@phosphor-icons/react';
+import { inferRouterOutputs } from '@trpc/server';
+
+type RouterOutput = inferRouterOutputs<AppRouter>;
+
+interface Props {
+    profileData?: RouterOutput['user']['getProfile'];
+    username: string;
+    isCurrentUser: boolean;
+}
+
+export const ProfileCard = ({ profileData, username, isCurrentUser }: Props) => {
+    return (
+        <div className="flex flex-col md:flex-row justify-between md:items-center border border-b-gray-500 p-10 gap-10 md:gap-0">
+            <div className='flex flex-col gap-10'>
+                <div className='flex items-center gap-10'>
+                    <div className='relative sm:w-32 sm:h-32 w-24 h-24'>
+                        <Image src="/avatar.webp" fill alt={`${username}'s profile image`} className='object-cover rounded-full' />
+                    </div>
+
+                    <div className='space-y-4'>
+                        <h1 className='sm:text-5xl text-4xl font-bold'>{profileData?.name}</h1>
+                        <h3 className='sm:text-2xl text-xl font-bold text-gray-400'>@{profileData?.username}</h3>
+                    </div>
+                </div>
+
+                <div className='flex gap-4 text-gray-400 text-2xl sm:text-xl sm:flex-row flex-col'>
+                    <div className='flex gap-4'>
+                        <div className='flex items-center gap-1'>
+                            <Person className='mt-1' />
+                            <span>2</span>
+                        </div>
+
+                        <div className='flex items-center gap-1'>
+                            <Hoodie className='mt-1' />
+                            <span>12</span>
+                        </div>
+                        <div className='flex items-center gap-1'>
+                            <TShirt className='mt-1' />
+                            <span>21</span>
+                        </div>
+                    </div>
+
+                    <div className='flex gap-4'>
+                        <div className='flex items-center gap-1'>
+                            <Pants className='mt-1' />
+                            <span>14</span>
+                        </div>
+                        <div className='flex items-center gap-1'>
+                            <Sneaker className='mt-1' />
+                            <span>5</span>
+                        </div>
+
+                        <div className='flex items-center gap-1'>
+                            <Watch className='mt-1' />
+                            <span>4</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className='flex flex-col gap-10 md:items-end'>
+                <div className='text-2xl flex gap-6'>
+                    <div className='text-center'>
+                        <h1 className='font-bold'>54</h1>
+                        <h3>images</h3>
+                    </div>
+
+                    <div className='text-center'>
+                        <h1 className='font-bold'>1.2k+</h1>
+                        <h3>likes</h3>
+                    </div>
+                </div>
+
+                <div className='flex gap-4 text-gray-400 font-semibold'>
+                    {isCurrentUser && <Link href={'/settings'} className='border border-gray-400 px-6 h-10 rounded-sm flex items-center justify-center'>Edit Profile</Link>}
+                    <button className='border border-gray-400 px-6 h-10 rounded-sm text-xl'>
+                        <Share />
+                    </button>
+                </div>
+            </div>
+        </div>
+    )
+}
--- src/pages/[username].tsx
+++ src/pages/[username].tsx
@@ -1,15 +1,13 @@
 import { GetServerSidePropsContext, InferGetServerSidePropsType, NextPage } from 'next';
 import { useSession } from 'next-auth/react';
-import Image from 'next/image';
-import Link from 'next/link';
 import { useRouter } from 'next/router';
 import superjson from 'superjson';
 import { Layout } from '~/components/Layout';
+import { ProfileCard } from '~/components/ProfileCard';
 import { appRouter } from '~/server/api/root';
 import { prisma } from '~/server/db';
 import { api } from '~/utils/api';
 
-import { Hoodie, Pants, Share, Sneaker, TShirt } from '@phosphor-icons/react';
 import { createServerSideHelpers } from '@trpc/react-query/server';
 
 export const ProfilePage = ({ username }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
@@ -18,66 +16,13 @@ export const ProfilePage = ({ username }: InferGetServerSidePropsType<typeof get
 
     const { data: profileData } = api.user.getProfile.useQuery({ username }, { onError: () => push('/') });
 
-    const isCurrentUser = data && data.user.username === username;
+    const isCurrentUser = data?.user.username === username ?? false;
 
     const pageTitle = isCurrentUser ? `profile` : username ?? 'profile';
 
     return (
         <Layout title={pageTitle}>
-            <div className="flex flex-col md:flex-row justify-between md:items-center border border-b-gray-500 p-10 gap-10 md:gap-0">
-                <div className='flex flex-col gap-10'>
-                    <div className='flex items-center gap-10'>
-                        <div className='relative w-32 h-32'>
-                            <Image src="/avatar.webp" fill alt={`${username}'s profile image`} className='object-cover rounded-full' />
-                        </div>
-
-                        <div className='space-y-4'>
-                            <h1 className='text-5xl font-bold'>{profileData?.name}</h1>
-                            <h3 className='text-2xl font-bold text-gray-400'>@{profileData?.username}</h3>
-                        </div>
-                    </div>
-
-                    <div className='flex gap-4 text-gray-400 text-xl'>
-                        <div className='flex items-center gap-1'>
-                            <Hoodie className='mt-1' />
-                            <span>12</span>
-                        </div>
-                        <div className='flex items-center gap-1'>
-                            <TShirt className='mt-1' />
-                            <span>21</span>
-                        </div>
-                        <div className='flex items-center gap-1'>
-                            <Pants className='mt-1' />
-                            <span>14</span>
-                        </div>
-                        <div className='flex items-center gap-1'>
-                            <Sneaker className='mt-1' />
-                            <span>5</span>
-                        </div>
-                    </div>
-                </div>
-
-                <div className='flex flex-col gap-10 md:items-end'>
-                    <div className='text-2xl flex gap-6'>
-                        <div className='text-center'>
-                            <h1 className='font-bold'>54</h1>
-                            <h3>images</h3>
-                        </div>
-
-                        <div className='text-center'>
-                            <h1 className='font-bold'>1.2k+</h1>
-                            <h3>likes</h3>
-                        </div>
-                    </div>
-
-                    <div className='flex gap-4 text-gray-400 font-semibold'>
-                        {isCurrentUser && <Link href={'/settings'} className='border border-gray-400 px-6 h-10 rounded-sm flex items-center justify-center'>Edit Profile</Link>}
-                        <button className='border border-gray-400 px-6 h-10 rounded-sm text-xl'>
-                            <Share />
-                        </button>
-                    </div>
-                </div>
-            </div>
+            <ProfileCard profileData={profileData} isCurrentUser={isCurrentUser} username={username} />
         </Layout>
     );
 };
