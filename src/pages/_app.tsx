4b507e101fab3113b83cf77fa3617745128ce3ba
Jeremy <68130184+Jecta@users.noreply.github.com>
2023-07-26 22:17:51 +0200

    Update _app.tsx
    hope this fixes it

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -19,10 +19,10 @@ const MyApp: AppType<{ session: Session | null }> = ({
     <>
       <Head>
         <meta charSet="UTF-8" />
-        <meta content="width=device-width, initial-scale=1.0" />
+        <meta property="viewport" content="width=device-width, initial-scale=1.0" />
         <title>{metadata.title}</title>
         <link rel="icon" href="favicon.ico" sizes="32px" />
-        <meta content={metadata.description} />
+        <meta property="description" content={metadata.description} />
         <meta property="og:locale" content="en_US" />
         <meta property="og:url" content={metadata.url} />
         <meta property="og:type" content={metadata.og.type} />
@@ -30,12 +30,12 @@ const MyApp: AppType<{ session: Session | null }> = ({
         <meta property="og:description" content={metadata.og.description} />
         <meta property="og:image" content={metadata.og.image} />
         <meta property="og:site_name" content={metadata.og.siteName} />
-        <meta username="twitter:card" content={metadata.twitter.card} />
-        <meta username="twitter:domain" content={metadata.twitter.domain} />
-        <meta username="twitter:url" content={metadata.twitter.url} />
-        <meta username="twitter:title" content={metadata.twitter.title} />
-        <meta username="twitter:description" content={metadata.twitter.description} />
-        <meta username="twitter:image" content={metadata.twitter.image} />
+        <meta property="twitter:card" content={metadata.twitter.card} />
+        <meta property="twitter:domain" content={metadata.twitter.domain} />
+        <meta property="twitter:url" content={metadata.twitter.url} />
+        <meta property="twitter:title" content={metadata.twitter.title} />
+        <meta property="twitter:description" content={metadata.twitter.description} />
+        <meta property="twitter:image" content={metadata.twitter.image} />
       </Head>
 
       <SessionProvider session={session}>
