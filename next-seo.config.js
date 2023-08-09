8ef51c95d4ea515bb3d3c6ae274d8bb2bbe7bc93
Jeremy <68130184+Jecta@users.noreply.github.com>
2023-08-09 20:40:01 +0200

    Update next-seo.config.js

--- next-seo.config.js
+++ next-seo.config.js
@@ -1,15 +1,15 @@
 export const metadata = {
   title: "outfits.bio",
   description:
-    "I'm building a virtual wardrobe where people can add photos to their profile and share them with a link like outfits.bio/jecta.",
+    "Outfits.bio is a digital platform where you can upload photos of your clothes and create your own personalized profile. It's as simple as snapping a picture, uploading it to your profile, and voila – you've got a sharable link like https://outfits.bio/jeremy. Stay trendy and connected with your friends by sharing your latest looks. 📸🎉",
   image: "https://outfits.bio/ogimage.png",
   url: "https://outfits.bio",
   og: {
     locale: "en_US",
     type: "website",
-    title: "Something drippy is cooking",
+    title: "outfits.bio",
     description:
-      "I'm building a virtual wardrobe where people can add photos to their profile and share them with a link like outfits.bio/jecta.",
+      "Outfits.bio is a digital platform where you can upload photos of your clothes and create your own personalized profile. It's as simple as snapping a picture, uploading it to your profile, and voila – you've got a sharable link like https://outfits.bio/jeremy. Stay trendy and connected with your friends by sharing your latest looks. 📸🎉",
     image: "https://outfits.bio/ogimage.png",
     imageWidth: 1200,
     imageHeight: 630,
@@ -19,9 +19,9 @@ export const metadata = {
     card: "summary_large_image",
     domain: "outfits.bio",
     url: "https://outfits.bio",
-    title: "Something drippy is cooking",
+    title: "outfits.bio",
     description:
-      "I'm building a virtual wardrobe where people can add photos to their profile and share them with a link like outfits.bio/jecta.",
+      "Outfits.bio is a digital platform where you can upload photos of your clothes and create your own personalized profile. It's as simple as snapping a picture, uploading it to your profile, and voila – you've got a sharable link like https://outfits.bio/jeremy. Stay trendy and connected with your friends by sharing your latest looks. 📸🎉",
     image: "https://outfits.bio/ogimage.png",
     imageWidth: 1200,
     imageHeight: 630,
