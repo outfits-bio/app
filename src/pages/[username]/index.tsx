7dd1c784932a427adcdfcf22221c3da6c5406b35
Brice Duke <bricewduke@gmail.com>
2023-08-17 17:14:28 -0500

    feat(global): fix

--- src/pages/[username]/index.tsx
+++ src/pages/[username]/index.tsx
@@ -1,10 +1,11 @@
 import { GetStaticProps, NextPage } from 'next';
 import { useSession } from 'next-auth/react';
 import { useRouter } from 'next/router';
-import { Fragment } from 'react';
+import { Fragment, useEffect, useState } from 'react';
 import { Layout } from '~/components/Layout';
 import { PostSection } from '~/components/PostSection';
 import { ProfileCard } from '~/components/ProfileCard';
+import { ProfilePost, ProfilePostModal } from '~/components/ProfilePostModal';
 import { generateSSGHelper } from '~/server/utils/ssg.util';
 import { api } from '~/utils/api.util';
 import { handleErrors } from '~/utils/handle-errors.util';
@@ -12,8 +13,11 @@ import { handleErrors } from '~/utils/handle-errors.util';
 import { PostType } from '@prisma/client';
 
 export const ProfilePage: NextPage<{ username: string }> = ({ username }) => {
-    const { push } = useRouter();
+    const { push, query } = useRouter();
     const { data, status } = useSession();
+
+    const [postModalOpen, setPostModalOpen] = useState<boolean>(false);
+    const [post, setPost] = useState<ProfilePost | null>(null);
 
     const { data: profileData, isLoading } = api.user.getProfile.useQuery({ username }, { onError: (e) => handleErrors({ e, message: "Failed to get user!", fn: () => push('/') }), retry: 0 });
 
@@ -21,12 +25,27 @@ export const ProfilePage: NextPage<{ username: string }> = ({ username }) => {
         id: profileData?.id ?? ''
     }, { retry: 0, enabled: !!profileData?.id, refetchOnMount: false, refetchOnWindowFocus: false, onError: (e) => handleErrors({ e, message: "Failed to get posts!", fn: () => push('/') }) });
 
+    useEffect(() => {
+        if (query.postId) {
+            const post = postsData?.find(p => p.id === query.postId);
+
+            if (post) {
+                setPost(post);
+                setPostModalOpen(true);
+            }
+        } else {
+            setPost(null);
+            setPostModalOpen(false);
+        }
+    }, [query.postId, postsData]);
+
     const isCurrentUser = data?.user.username === username ?? false;
 
     const pageTitle = isCurrentUser ? `profile` : username ?? 'profile';
 
     return (
         <Layout title={pageTitle}>
+            {postModalOpen && <ProfilePostModal setPostModalOpen={setPostModalOpen} post={post} user={profileData ?? null} />}
             <div className='flex flex-col md:flex-row w-screen h-full gap-4 p-4 md:gap-12 md:p-12 overflow-y-scroll md:overflow-y-auto'>
                 <ProfileCard loading={isLoading} authStatus={status} currentUser={data?.user ?? null} profileData={profileData} username={username} isCurrentUser={isCurrentUser} />
 
