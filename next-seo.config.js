17f9981faff6dfed9e63f2f429b9cca778071e54
Jeremy <68130184+Jecta@users.noreply.github.com>
2023-09-05 21:18:33 +0200

    Revert next-seo.config.js

--- next-seo.config.js
+++ next-seo.config.js
@@ -7,7 +7,6 @@ export const metadata = {
   og: {
     locale: "en_US",
     type: "website",
-    themeColor: '#FF264D',
     title: "outfits.bio",
     description:
       "outfits is a virtual wardrobe where you can seamlessly share and explore outfits with your friends and viewers!",
