e741259fec5cebe8364297cd023bccd7d870f7f9
Jeremy Bosma <jectaunscripted@gmail.com>
2023-12-08 23:36:43 +0100

    Update metadata in layout.tsx

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -12,6 +12,29 @@ import { api } from '~/utils/api.util';
 export const metadata = {
   title: meta.title,
   description: meta.description,
+  image: meta.image,
+  url: meta.url,
+  og: {
+    locale: meta.og.locale,
+    type: meta.og.type,
+    title: meta.og.title,
+    description: meta.og.description,
+    image: meta.og.image,
+    embedColor: meta.og.embedColor,
+    imageWidth: meta.og.imageWidth,
+    imageHeight: meta.og.imageHeight,
+    siteName: meta.og.siteName,
+  },
+  twitter: {
+    card: meta.twitter.card,
+    domain: meta.twitter.domain,
+    url: meta.twitter.url,
+    title: meta.twitter.title,
+    description: meta.twitter.description,
+    image: meta.twitter.image,
+    imageWidth: meta.twitter.imageWidth,
+    imageHeight: meta.twitter.imageHeight,
+  },
 }
 
 export default function RootLayout({
