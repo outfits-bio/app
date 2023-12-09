0b08c9fb2d5f8c194590de170d0ff186a9c40d3b
Jeremy Bosma <jectaunscripted@gmail.com>
2023-12-09 15:49:30 +0100

    Add additional themes to NextThemeProvider

--- apps/web/src/components/ThemeProvider.tsx
+++ apps/web/src/components/ThemeProvider.tsx
@@ -2,7 +2,7 @@ import { ThemeProvider as NextThemesProvider } from 'next-themes';
 
 export function NextThemeProvider({ children }: { children: React.ReactNode }) {
     return (
-        <NextThemesProvider attribute="class" defaultTheme="light" themes={['light', 'dark']}>
+        <NextThemesProvider attribute="class" defaultTheme="light" themes={['light', 'dark', 'light-brown', 'light-hot-pink', 'light-orange', 'light-light-pink']}>
             {children}
         </NextThemesProvider>
     );
