a9612c1e37cd569ac1bcc232b2a0b5635e74f6ba
Jeremy <jectaunscripted@gmail.com>
2024-02-17 22:07:51 +0100

    Update scroll bar width and height in CSS

--- apps/web/src/app/[username]/page.tsx
+++ apps/web/src/app/[username]/page.tsx
@@ -7,7 +7,7 @@ export default async function ProfilePage({ params }: { params: { username: stri
     try {
         const profileData = await api.user.getProfile.query({ username: params.username });
 
-        return <div className='flex flex-col md:flex-row w-screen h-full gap-4 pr-4 md:gap-20 lg:gap-36  overflow-y-scroll md:overflow-y-auto'>
+        return <div className='flex flex-col md:flex-row w-screen h-full gap-4 pr-4 md:gap-20 lg:gap-36  overflow-y-scroll-auto md:overflow-y-auto'>
             <ProfileCard profileData={profileData} username={params.username} />
 
             <Posts profileData={profileData} />
