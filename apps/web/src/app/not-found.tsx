65060d0db29b69787e8e18b9f9e92ddbad583222
Jeremy Bosma <jectaunscripted@gmail.com>
2024-06-01 01:48:58 +0200

    404 page

---
+++ apps/web/src/app/not-found.tsx
@@ -0,0 +1,11 @@
+import { Button } from "./_components/ui/Button"
+import Link from "next/link";
+
+export default async function DiscoverPage() {
+    return <div className="flex flex-col h-full w-full justify-center items-center gap-3">
+        <h1 className="font-clash font-bold text-6xl">Page Not Found</h1>
+        <Link href="javascript:window.history.back();">
+            <Button variant="primary" className="w-fit">Go Back</Button>
+        </Link>
+    </div>;
+}
