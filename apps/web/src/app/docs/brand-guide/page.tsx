113069630f7a47d19538d09e5a5ca35e21cd2014
Jeremy Bosma <jectaunscripted@gmail.com>
2024-06-01 01:52:26 +0200

    legal documents

---
+++ apps/web/src/app/docs/brand-guide/page.tsx
@@ -0,0 +1,5 @@
+export const BrandGuidePage = () => {
+    return <iframe title='Brand Guide' src='/brand.pdf' className='w-full xl:w-[1000px] h-screen -mt-20 pt-24 px-4 pb-4' />
+}
+
+export default BrandGuidePage;
