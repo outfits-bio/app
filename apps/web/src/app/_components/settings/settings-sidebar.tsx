7970b90f7c798bb56a37001601a362b42d83efde
Jeremy Bosma <jectaunscripted@gmail.com>
2024-06-01 14:53:56 +0200

    fix settings pages not being accessible

--- apps/web/src/app/_components/settings/settings-sidebar.tsx
+++ apps/web/src/app/_components/settings/settings-sidebar.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import Link from 'next/link';
 import { Button } from "@/app/_components/ui/Button";
 import { signOut } from 'next-auth/react';
--- apps/web/src/app/not-found.tsx
+++ apps/web/src/app/not-found.tsx
@@ -1,7 +1,7 @@
 import { Button } from "./_components/ui/Button"
 import Link from "next/link";
 
-export default async function DiscoverPage() {
+export default async function NotFoundPage() {
     return <div className="flex flex-col h-full w-full justify-center items-center gap-3">
         <h1 className="font-clash font-bold text-6xl">Page Not Found</h1>
         <Link href="javascript:window.history.back();">
