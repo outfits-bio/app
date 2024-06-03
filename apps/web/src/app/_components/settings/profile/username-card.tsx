e4f357865874497425445b03b9eba7ef28ae551a
Jess <jessdaniel474@gmail.com>
2024-06-03 19:00:43 -0400

    adds username setting

--- apps/web/src/app/_components/settings/profile/username-card.tsx
+++ apps/web/src/app/_components/settings/profile/username-card.tsx
@@ -1,28 +1,80 @@
 "use client";
-import { Button } from "../../ui/Button"
+import { useState } from "react";
+import { useForm } from "react-hook-form";
 import { useSession } from "next-auth/react";
+import { Button } from "../../ui/Button"
+import { api } from "@/trpc/react";
+import {
+    EditProfileInput, editProfileSchema
+} from '@/schemas/user.schema';
+import { zodResolver } from '@hookform/resolvers/zod';
+import toast from "react-hot-toast";
+import { handleErrors } from '@/utils/handle-errors.util';
 
 export function UsernameCard() {
-    const { data: session } = useSession();
+    const { data: session, update } = useSession();
+    const { register, handleSubmit, setValue, formState: { errors }, setError, clearErrors } = useForm<EditProfileInput>({
+        resolver: zodResolver(editProfileSchema),
+    });
+    const [loading, setLoading] = useState<boolean>(false);
+    const { data: userData } = api.user.getMe.useQuery(undefined, {
+        onSuccess: (data) => {
+            setValue("username", data.username ?? '');
+        }
+    });
+
+    const { mutate } = api.user.editProfile.useMutation({
+        onSuccess: async (data) => {
+            await update();
+            toast.success("Username updated!")
+        },
+        onError: (e) => handleErrors({ e, message: "Failed to edit profile!", fn: () => setLoading(false) })
+    });
+
+    const handleFormSubmit = ({ username }: EditProfileInput) => {
+        setLoading(true);
+
+        if (username === session?.user?.username) {
+            setError("username", {
+                type: "manual",
+                message: "Your new username must be different from your current username."
+            })
+            setLoading(false);
+            return;
+        } else {
+            clearErrors("username")
+        }
+
+        // If the user didn't change their username, do nothing
+        if (username?.length) mutate({
+            username
+        });
+        else {
+            setLoading(false);
+        }
+    };
 
     return (
         <div className="flex flex-col items-start rounded-lg border bg-white">
-            <div className="flex flex-col items-start flex gap-5 p-10 self-stretch">
-                <div className="flex flex-col items-start gap-3 flex-1">
-                    <h1 className="font-clash font-bold text-3xl">Username</h1>
-                    <p>This is how you will appear everywhere on the platform. Put whatever you are comfortable with.</p>
+            <form className="self-stretch" onSubmit={handleSubmit(handleFormSubmit)}>
+                <div className="flex flex-col items-start flex gap-5 p-10 self-stretch">
+                    <div className="flex flex-col items-start gap-3 flex-1">
+                        <h1 className="font-clash font-bold text-3xl">Username</h1>
+                        <p>This is how you will appear everywhere on the platform. Put whatever you are comfortable with.</p>
+                    </div>
+                    <div className="flex justify-between items-center self-stretch border rounded-lg">
+                        <div className="flex p-3 items-center gap-10 self-stretch bg-gray-100">outfits.bio/</div>
+                        <input {...register("username", { maxLength: 24 })} className="flex items-center gap-4 p-3 py-4 flex-1 self-stretch" placeholder={session?.user?.username ?? "username"} />
+                    </div>
+                    <div>{errors.username && <p className="text-red-500 text-sm">{errors.username.message}</p>}</div>
                 </div>
-                <div className="flex justify-between items-center self-stretch border rounded-lg">
-                    <div className="flex p-3 items-center gap-10 self-stretch bg-gray-100">outfits.bio/</div>
-                    <input className="flex items-center gap-4 p-3 py-4 flex-1 self-stretch" placeholder={session?.user?.username ?? "username"} />
+                <div className="flex flex-wrap items-center gap-3 p-4 px-10 self-stretch justify-between border-t bg-gray-100">
+                    <p>Your username can only have up to 24 characters.</p>
+                    <div className="flex items-center gap-3">
+                        <Button type="submit" >Save</Button>
+                    </div>
                 </div>
-            </div>
-            <div className="flex flex-wrap items-center gap-3 p-4 px-10 self-stretch justify-between border-t bg-gray-100">
-                <p>You can only change your username every 30 days, and can only have up to 24 characters.</p>
-                <div className="flex items-center gap-3">
-                    <Button>Save</Button>
-                </div>
-            </div>
+            </form>
         </div>
     )
 }
