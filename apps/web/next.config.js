fe4a58f57365b30f70f85fb5c2afdb517636003c
Jeremy Bosma <jectaunscripted@gmail.com>
2024-06-19 21:23:33 +0200

    try fix env

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -8,6 +8,7 @@
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
  */
+await import("./src/env.js");
 import { PrismaPlugin } from "@prisma/nextjs-monorepo-workaround-plugin";
 
 /** @type {import("next").NextConfig} */
