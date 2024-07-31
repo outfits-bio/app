56437b0aa86378cb747b9b1cb231ea22f7b77467
Jeremy Bosma <jectaunscripted@gmail.com>
2024-07-31 22:04:08 +0200

    fix padding

--- apps/web/src/components/navigation/mobile-nav.tsx
+++ apps/web/src/components/navigation/mobile-nav.tsx
@@ -19,7 +19,7 @@ export function MobileNav() {
 
     if (pathname !== '/login' && pathname !== '/onboarding') {
         return (
-            <div className="flex w-full h-21 pt-1 justify-between items-center px-6 border-t gap-3 sm:hidden fixed bottom-0 left-0 right-0 bg-white bg-opacity-95 dark:bg-black dark:border-stroke mobilenav">
+            <div className="flex w-full h-21 pt-2 pb-2 justify-between items-center px-6 border-t gap-3 sm:hidden fixed bottom-0 left-0 right-0 bg-white bg-opacity-95 dark:bg-black dark:border-stroke mobilenav">
                 <Link href={'/'} className='rounded-xl flex items-center justify-center text-3xl transform transition duration-100 ease-in-out active:scale-[110%]'>
                     {pathname === '/' ? <PiHouseFill /> : <PiHouse />}
                 </Link>
