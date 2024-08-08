c29c79a321fd1b52e49817f49f3a99c8a7a8b4fd
Jeremy Bosma <jectaunscripted@gmail.com>
2024-08-08 22:59:51 +0200

    make register banner look better

--- apps/nextjs/src/components/discover/register-banner.tsx
+++ apps/nextjs/src/components/discover/register-banner.tsx
@@ -7,7 +7,7 @@ export function RegisterBanner() {
 
     if (!session) {
         return (
-            <div className="bg-white dark:bg-black hidden flex-wrap w-full h-fit justify-between items-center p-5 px-4 border-t rounded-t-lg gap-3 md:flex fixed bottom-0 left-0 right-0">
+            <div className="bg-white dark:bg-black hidden flex-wrap w-full h-fit justify-between items-center p-5 px-4 border-t rounded-t-lg gap-3 md:flex">
                 <div className="flex flex-col items-start gap-3">
                     <h1 className="font-clash text-2xl font-bold">Create your profile and start posting!</h1>
                     <p>Creating a profile is super easy and removes all limitations you might experience on this page right now.</p>
