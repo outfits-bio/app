9542ba601b1dd789ff8e3aa353bd972d3009fd2a
Jeremy Bosma <jectaunscripted@gmail.com>
2024-08-21 20:29:27 +0200

    change login page to only render outfits

--- packages/api/src/routers/post.ts
+++ packages/api/src/routers/post.ts
@@ -694,7 +694,11 @@ export const postRouter = createTRPCRouter({
     const orderDirection = Math.random() > 0.5 ? "asc" : "desc";
 
     const posts = await ctx.db.post.findMany({
-      where: {},
+      where: {
+        type: {
+          in: ["OUTFIT"],
+        },
+      },
       select: {
         id: true,
         image: true,
