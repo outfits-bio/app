938eb155433cf1eb80e6378a2d4bc66d1d81a0fa
Jeremy Bosma <jectaunscripted@gmail.com>
2024-08-24 23:15:53 +0200

    database fix

---
+++ packages/db/src/prisma/migrations/20240824211521_more_social_features/migration.sql
@@ -0,0 +1,71 @@
+-- AlterEnum
+-- This migration adds more than one value to an enum.
+-- With PostgreSQL versions 11 and earlier, this is not possible
+-- in a single migration. This can be worked around by creating
+-- multiple migrations, each migration adding only one value to
+-- the enum.
+
+
+ALTER TYPE "NotificationType" ADD VALUE 'POST_WISHLIST';
+ALTER TYPE "NotificationType" ADD VALUE 'POST_COMMENT';
+ALTER TYPE "NotificationType" ADD VALUE 'COMMENT_REPLY';
+ALTER TYPE "NotificationType" ADD VALUE 'COMMENT_LIKE';
+
+-- AlterTable
+ALTER TABLE "Post" ADD COLUMN     "caption" TEXT,
+ADD COLUMN     "productLink" TEXT,
+ADD COLUMN     "tags" TEXT[];
+
+-- CreateTable
+CREATE TABLE "Comment" (
+    "id" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "userId" TEXT NOT NULL,
+    "postId" TEXT NOT NULL,
+    "parentId" TEXT,
+
+    CONSTRAINT "Comment_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "CommentLike" (
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "userId" TEXT NOT NULL,
+    "commentId" TEXT NOT NULL,
+
+    CONSTRAINT "CommentLike_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Subscription" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "endpoint" TEXT NOT NULL,
+    "keys" JSONB NOT NULL,
+
+    CONSTRAINT "Subscription_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE INDEX "Comment_userId_idx" ON "Comment"("userId");
+
+-- CreateIndex
+CREATE INDEX "Comment_postId_idx" ON "Comment"("postId");
+
+-- CreateIndex
+CREATE INDEX "Comment_parentId_idx" ON "Comment"("parentId");
+
+-- CreateIndex
+CREATE INDEX "CommentLike_userId_idx" ON "CommentLike"("userId");
+
+-- CreateIndex
+CREATE INDEX "CommentLike_commentId_idx" ON "CommentLike"("commentId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "CommentLike_userId_commentId_key" ON "CommentLike"("userId", "commentId");
+
+-- CreateIndex
+CREATE INDEX "Subscription_userId_idx" ON "Subscription"("userId");
