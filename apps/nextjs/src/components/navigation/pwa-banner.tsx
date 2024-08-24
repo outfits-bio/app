5b665ea9a39567ea2271be913c8ff63b37484f2b
Jeremy Bosma <jectaunscripted@gmail.com>
2024-08-24 15:53:23 +0200

    try fixing pwa banner

--- apps/nextjs/src/components/navigation/pwa-banner.tsx
+++ apps/nextjs/src/components/navigation/pwa-banner.tsx
@@ -3,17 +3,18 @@
 import { useEffect, useState } from "react";
 import { Button } from "../ui/Button";
 import toast from "react-hot-toast";
+import Image from "next/image";
 
 export function PwaBanner() {
     const [isIOS, setIsIOS] = useState(true);
     const [isStandalone, setIsStandalone] = useState(false);
 
-    // useEffect(() => {
-    //     const ua = window.navigator.userAgent;
-    //     const iOS = !!ua.match(/iPad/i) || !!ua.match(/iPhone/i);
-    //     setIsIOS(iOS);
-    //     setIsStandalone(window.matchMedia('(display-mode: standalone)').matches);
-    // }, []);
+    useEffect(() => {
+        const ua = window.navigator.userAgent;
+        const iOS = !!ua.match(/iPad/i) || !!ua.match(/iPhone/i);
+        setIsIOS(iOS);
+        setIsStandalone(window.matchMedia('(display-mode: standalone)').matches);
+    }, []);
 
     function openNavigator() {
         if (navigator.share) {
@@ -31,10 +32,11 @@ export function PwaBanner() {
 
     if (isIOS && !isStandalone) {
         return (
-            <section className="z-50 gap-5 flex-col h-screen w-screen bg-white dark:bg-black text-black dark:text-white flex justify-center items-center px-6 text-center">
+            <section className="z-50 justify-between flex-col h-screen w-screen bg-white dark:bg-black text-black dark:text-white flex py-10 items-center px-6 text-center">
 
-                <div className="flex flex-col gap-1">
-                    <p className="text-2xl font-semibold font-clash">Install the website as an app!</p>
+                <div className="flex flex-col gap-1 items-center">
+                    <Image src="/apple-touch-icon.png" alt="Outfits.bio logo" width={100} height={100} loading="eager" priority />
+                    <p className="text-2xl font-semibold font-clash">Outfits.bio is better as an app!</p>
                     <p>Installing the website as an app will make it behave like any app you're used to.</p>
                 </div>
 
