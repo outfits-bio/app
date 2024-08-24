f46dc68704bbe91d8eb258be59f3fd8685ea1157
Jeremy Bosma <jectaunscripted@gmail.com>
2024-08-24 13:16:18 +0200

    suggested for you on mobile

--- apps/nextjs/src/components/discover/discover-content.tsx
+++ apps/nextjs/src/components/discover/discover-content.tsx
@@ -16,6 +16,7 @@ import { RegisterBanner } from './register-banner'
 import { Avatar } from '../ui/Avatar'
 import { motion, AnimatePresence } from 'framer-motion'
 import { BaseModal, BaseModalContent, BaseModalHeader, BaseModalTitle, BaseModalTrigger } from '../modals/base-modal'
+import { PopularProfiles } from './popular-profiles'
 
 export function DiscoverContent({ initialPosts, popularProfiles }: { initialPosts: any; popularProfiles: any }) {
     const params = useSearchParams()
@@ -330,40 +331,12 @@ export function DiscoverContent({ initialPosts, popularProfiles }: { initialPost
                             : null}
                     </div>
 
+
+
                     {/* Popular Profiles */}
-                    {popularProfilesData && (
-                        <div className="hidden xl:flex flex-col gap-3 pt-[1rem] h-fit w-80 mr-8">
-                            <h2 className="text-sm font-semibold text-secondary-text">
-                                Suggested for you
-                            </h2>
-                            <div className="flex flex-col gap-3">
-                                {popularProfilesData?.map((user) => (
-                                    <div key={user?.id ?? ''} className="flex items-center justify-between">
-                                        <Link href={`/${user?.username}`} className="flex items-center gap-3">
-                                            <Avatar
-                                                size={'sm'}
-                                                image={user?.image}
-                                                id={user?.id}
-                                                username={user?.username}
-                                            />
-                                            <div className="flex flex-col">
-                                                <span className="font-semibold text-sm text-black dark:text-white">{user?.username}</span>
-                                                <span className="text-xs text-secondary-text text-nowrap">
-                                                    {user?.imageCount} Shots · {user?.likeCount} Likes
-                                                </span>
-                                            </div>
-                                        </Link>
-                                        <Link
-                                            href={`/${user?.username}`}
-                                            className="text-sm font-semibold text-orange-accent text-center p-0 ml-5 cursor-pointer"
-                                        >
-                                            View
-                                        </Link>
-                                    </div>
-                                ))}
-                            </div>
-                        </div>
-                    )}
+                    <div className="hidden xl:flex w-80 mr-8">
+                        <PopularProfiles />
+                    </div>
                 </div>
 
                 <RegisterBanner />
