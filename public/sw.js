0585df2b9b79e6d37b8b4a1a7e2e4a27bc91754d
Jeremy Bosma <jectaunscripted@gmail.com>
2024-08-29 17:22:32 +0200

    try adding offline to pwa

--- public/sw.js
+++ public/sw.js
@@ -3,23 +3,46 @@
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 /* eslint-disable @typescript-eslint/ban-ts-comment */
 // @ts-nocheck
-self.addEventListener("push", (event) => {
-  if (event.data) {
-    try {
-      const data = event.data.json();
-      const options = {
-        body: data.body,
-        icon: "/icon-192x192.png",
-      };
 
-      event.waitUntil(self.registration.showNotification(data.title, options));
-    } catch (error) {
-      console.error("Error processing push event:", error);
+// Push notifications
+
+self.addEventListener("push", (event) => {
+    if (event.data) {
+        try {
+            const data = event.data.json();
+            const options = {
+                body: data.body,
+                icon: "/icon-192x192.png",
+            };
+
+            event.waitUntil(self.registration.showNotification(data.title, options));
+        } catch (error) {
+            console.error("Error processing push event:", error);
+        }
     }
-  }
 });
 
 self.addEventListener("notificationclick", (event) => {
-  event.notification.close();
-  event.waitUntil(clients.openWindow("/"));
+    event.notification.close();
+    event.waitUntil(clients.openWindow("/"));
 });
+
+
+// This is the "Offline copy of pages" service worker
+
+const CACHE = "pwabuilder-offline";
+
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+
+self.addEventListener("message", (event) => {
+    if (event.data && event.data.type === "SKIP_WAITING") {
+        self.skipWaiting();
+    }
+});
+
+workbox.routing.registerRoute(
+    new RegExp('/*'),
+    new workbox.strategies.StaleWhileRevalidate({
+        cacheName: CACHE
+    })
+);
